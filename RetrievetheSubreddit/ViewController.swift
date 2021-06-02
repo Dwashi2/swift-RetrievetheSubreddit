f5ce0db613a35c0bc3f6fef92137f41809fd8887
Daniel Washington <danielvertigo@hotmail.com>
2021-06-02 09:36:44 -0300

    Files

--- RetrievetheSubreddit/ViewController.swift
+++ RetrievetheSubreddit/ViewController.swift
@@ -5,6 +5,19 @@
 //  Created by Daniel Washington Ignacio on 02/06/21.
 //
 
+
+/*
+ Create a function to extract the name of the subreddit from its URL.
+
+ Examples
+
+ subReddit("https://www.reddit.com/r/funny/") ➞ "funny"
+
+ subReddit("https://www.reddit.com/r/relationships/") ➞ "relationships"
+
+ subReddit("https://www.reddit.com/r/mildlyinteresting/") ➞ "mildlyinteresting"
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +25,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //print(self.subReddit("https://www.reddit.com/r/funny/"))
+        //print(self.subReddit("https://www.reddit.com/r/relationships/"))
+        print(self.subReddit("https://www.reddit.com/r/mildlyinteresting/"))
     }
 
+    func subReddit(_ link: String) -> String {
+        var last: [String] = []
+        last = link.components(separatedBy: "/")
+        last.removeLast()
+        return (last.last ?? "")
+    }
 
 }
 
